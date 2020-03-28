6465b8ea1900bf0c0af53d04dc2f2452290b4b1e
ZAOUAM Sirageddine <zaouam.sirageddine@hotmail.com>
2020-03-28 18:40:02 +0100

    Création de Robot_Volant et Robot_Lent

---
+++ src/main/java/Robot_Lent.java
@@ -0,0 +1,12 @@
+
+public class Robot_Lent extends Robot {
+	
+	public Robot_Lent(int position) {
+		super(position);
+	}
+public void avance()
+{
+	super.position+=2;
+}
+
+}
---
+++ src/main/java/Robot_volant.java
@@ -0,0 +1,18 @@
+
+public class Robot_volant extends Robot{
+	
+	public Robot_volant(int position) {
+		super(position);
+	}
+public void avance()
+{
+	throw new UnsupportedOperationException();}
+
+
+
+public void Voler()
+{
+	System.out.println("Le robot vole  :) !");
+	
+}
+}
