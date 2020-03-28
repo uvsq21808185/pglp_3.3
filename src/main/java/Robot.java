d105dbd5604732cb4c0113bdde5259bbf69dcb17
ZAOUAM Sirageddine <zaouam.sirageddine@hotmail.com>
2020-03-28 10:12:56 +0100

    Creéation de la classe Robot et RobotStatique

---
+++ src/main/java/Robot.java
@@ -0,0 +1,18 @@
+import javax.swing.text.Position;
+
+public class Robot {
+	
+	public int position;
+	
+	public Robot(int position) {
+		this.position=position;
+	}
+	
+	public void avance()
+	{
+		this.position+=10;
+		
+	}
+	
+
+}
---
+++ src/main/java/RobotStatique.java
@@ -0,0 +1,12 @@
+
+public class RobotStatique {
+	
+	private int position;
+	public RobotStatique(int position) {
+		this.position=position;
+	}
+	public void avance() {
+		throw new UnsupportedOperationException();
+	}
+
+}
