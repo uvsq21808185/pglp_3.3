149abfc90f38bf778c5c292ec7f1f077ae77d434
ZAOUAM Sirageddine <zaouam.sirageddine@hotmail.com>
2020-03-28 18:40:36 +0100

    main

---
+++ src/main/java/main.java
@@ -0,0 +1,15 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Robot_volant robot1 = new Robot_volant(10);
+		
+		robot1.Voler();
+		robot1.avancerTous();
+		robot1.avance();
+		
+	}
+
+}
